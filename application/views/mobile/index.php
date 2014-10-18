83a1ed013b3e4cdedddb3299656f0a4e6db2341f
Traianovschi Andrei <andrei.traianovschi@newhaircut.com>
2014-10-18 22:34:05 +0300

    Added to render mobile pages in mobile/index.phtml

--- application/views/mobile/index.php
+++
@@ -1,7 +0,0 @@
-<?php $app->render('mobile/partials/head-includes.php') ?>
-
-<div class="wrapper">
-    <div id="map-canvas"></div>
-</div>
-
-<?php $app->render('mobile/partials/footer-includes.php'); ?>
