1fdc41ae7e5fddb72a328d7d6e72c3cdd4380161
Robert Kis <robert.kis@digitalxbridge.com>
2014-10-19 06:50:11 +0300

    Success page stylization

--- public/media/js/mobile/awesome.button.js
+++ public/media/js/mobile/awesome.button.js
@@ -3,5 +3,15 @@ function initializeAwesomeButton() {
 
     $('#button-awesome').click(function () {
         myDataRef.push({latitude: currentLocation.latitude, longitude: currentLocation.longitude, dateAdded: Date.now()});
+        $(".popup-window").show();
+        $(".refresh-btn").hide();
+        $(this).text('THANK YOU!');
+        $(this).css('box-shadow','none')
+    });
+    $('.close-popup-window').click(function() {
+        $(".popup-window").hide();
+        $(".refresh-btn").show();
+        $('#button-awesome').text('AWESOME');
+        $('#button-awesome').css('box-shadow','');
     });
 }
