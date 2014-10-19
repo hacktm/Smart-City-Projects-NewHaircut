2bc36572e441a7a4bab3b9ca2be7ecb3826ee213
Traianovschi Andrei <andrei.traianovschi@newhaircut.com>
2014-10-19 03:22:46 +0300

    Fixed me-pin image

--- public/media/js/mobile/map.js
+++ public/media/js/mobile/map.js
@@ -95,7 +95,7 @@ function setCurrentPostion() {
     var myLatlng = new google.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
     var marker = new google.maps.Marker({
         position: myLatlng,
-        icon: '/media/images/mobile/current-location-marker.png'
+        icon: '/media/images/mobile/me-pin.png'
     });
 
     marker.setMap(map);
